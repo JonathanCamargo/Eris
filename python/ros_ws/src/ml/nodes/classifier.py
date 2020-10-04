8dbdf5e9543f12a7a7f4ebefdebe2d41b38c56b9
ossip <jcl00087@gmail.com>
2020-10-04 11:22:08 -0400

    classifier

--- python/ros_ws/src/ml/nodes/classifier.py
+++ python/ros_ws/src/ml/nodes/classifier.py
@@ -28,6 +28,8 @@ from sklearn.model_selection import KFold
 from joblib import dump, load
 from copy import deepcopy
 
+from scipy import io
+
 ##################### ROS MESSAGES AND PUBLISHERS ##############################
 stringmsg=String()
 std_stringmsg=StdString()
@@ -41,10 +43,10 @@ lock=threading.Lock()
 learning = False
 
 MAX_SAMPLES=100 #Number of samples per class to hold in memory
-RETRAIN_SAMPLES=10 # Retrain model every RETRAIN_SAMPLES
 size=None #Size of the feature vector
 memory=dict() #Sample data
 numSamples=dict() #Number of samples
+VERBOSE=2
 
 # Setup a Rosbag
 path=os.path.join(os.environ['HOME'],date.today().strftime('%m_%d_%y'))
@@ -80,7 +82,7 @@ def label_callback(msg):
         numSamples[label]=0
         active_model=None #Reset the model since the number of labels changed
     lock.release()
-    
+
 def labelstd_callback(msg):
     stringmsg.header.stamp=rospy.Time.now()
     stringmsg.data=msg.data
@@ -163,10 +165,26 @@ def retrain(memory):
 def train(memory):
     lr=0.05
     tol=0.001
-    mdl=MLPClassifier(hidden_layer_sizes=(10,10),max_iter=300,learning_rate_init=lr,tol=tol)
+    mdl=MLPClassifier(hidden_layer_sizes=(10,10),max_iter=300,learning_rate_init=lr,tol=tol,verbose=VERBOSE)
     x,y=memory2xy(memory)
     mdl.fit(x,y)
     return mdl
+
+def CheckPoint():
+    global active_model,memory
+    '''Save a copy of the current model and the data in memory'''
+    modelfiles=f.fileList({'Type':'model','File':'*.mdl'})
+    nextmodel=f.genList({'Type':'model','File':'{:01d}.mdl'.format(len(modelfiles)+1)})
+    nextmemory=f.modFileList(nextmodel,{'Type':'memory','ext':'mat'})
+    #Save the model
+
+    printAndLog('Model checkpoint ({})'.format(nextmodel[0]))
+    print(nextmemory)
+    mkdirfile(nextmemory[0])
+    mkdirfile(nextmodel[0])
+    dump(active_model,nextmodel[0])
+    io.savemat(nextmemory[0],{'data':memory})
+
 ################################################################################
 ''' Main loop'''
 rospy.init_node('classifier', anonymous=True)
@@ -184,28 +202,42 @@ elapsed=0
 lasttime=rospy.Time.now()
 
 # Restore from previous model
-models=f.fileList({'Type':'models','File':'*.mdl'})
+models=f.fileList({'Type':'model','File':'*.mdl'})
 
 if len(models)>0:
    print('Previous models found:\n\t{}'.format(models))
+   memoryfiles=f.modFileList(models,{'Type':'memory','ext':'mat'})
    active_model=load(models[-1])
+   memory=io.loadmat(memoryfiles[-1])
+   print(memory.keys())
+
 
 count=0
 while True:
     time=rospy.Time.now()
-    printAndLog('label={}'.format(label))
-    for l in labels:
-        if count % 2 == 0:
-            x,y=memory2xy(memory)
-            if active_model==None:
-                mdl=train(memory)
-                lock.acquire()
-                active_model=deepcopy(mdl)
-                lock.release()
-            elif learning:
-                lock.acquire()
-                active_model=retrain(memory)
-                lock.release()
+    if ((not label==None) and (label in numSamples.keys())):
+        printAndLog('label={}({}samples)'.format(label,numSamples[label]))
+
+    if labels==[]:
+        rate.sleep()
+        continue
+
+    n=[numSamples[l] for l in labels]
+    print(n)
+    if (learning and (count % 5 == 0)):
+        if (active_model==None) and (np.all(np.greater(n,MAX_SAMPLES))):
+            mdl=train(memory)
+            lock.acquire()
+            active_model=deepcopy(mdl)
+            lock.release()
+            CheckPoint()
+        elif active_model!=None:
+            mdl=retrain(memory)
+            lock.acquire()
+            active_model=mdl
+            lock.release()
+            CheckPoint()
+
     count=count+1
 
     #Wait a second
