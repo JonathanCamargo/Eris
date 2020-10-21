3831fbf4ee68ec5bb2822e97b677801801e59fdd
Jonathan <jcl00087@gmail.com>
2020-10-21 02:49:22 -0400

    ErisBici
    Eris Flavor cor controlling PWM of an ebike

---
+++ Firmware/Flavors/ErisBici/streaming.h
@@ -0,0 +1,29 @@
+
+#ifndef STREAMING_H
+#define STREAMING_H
+
+#include <Arduino.h>
+#include "Eris.h"
+/*
+All the functions related to streaming of data
+*/
+
+
+namespace Streaming{
+
+bool AddFunction(char * arg);
+void ClearFunctions();
+
+//Functions should be declared as void and send the desired data via serial
+
+// Sinusoidal wave
+void SineWave();
+
+void Sync();
+
+void Stream();
+
+}
+
+
+#endif
