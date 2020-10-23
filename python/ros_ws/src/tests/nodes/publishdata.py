031091ad3418da8bbd5e7782b88603ed669efc86
Jonathan <jcl00087@gmail.com>
2020-10-23 01:25:34 -0400

    enable www

---
+++ python/ros_ws/src/tests/nodes/publishdata.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# Publish random data
+import rospy
+import rostopic
+import signal
+import sys
+import rospkg
+
+from rospy import Publisher
+from std_msgs.msg import MultiArrayDimension,Float32
+from std_msgs.msg import Float32MultiArray as Float32MultiArrayStd
+from custom_msgs.msg import Float32MultiArray
+
+from featureextraction.extractorhelper import *
+
+
+# Publishers and subscribers
+featurespub=Publisher('/features',Float32MultiArray,queue_size=1)
+featuresstdpub=Publisher('/featuresstd',Float32MultiArrayStd,queue_size=1)
+
+# Messages
+featuresmsg=Float32MultiArray()
+featuresmsgstd=Float32MultiArrayStd()
+
+############################ Signal term ######################################
+def signal_handler(sig,frame):
+    print('Ctrl+c')
+    sys.exit(0)
+signal.signal(signal.SIGINT,signal_handler)
+
+###################### Publishing and callback functions ###################
+def publishFeatures(sample):
+    featuresmsg.header.stamp=rospy.Time.now()
+    featuresmsg.data=sample
+    featuresmsg.layout.dim=[MultiArrayDimension()]
+    featuresmsg.layout.dim[0].size=len(sample)
+    featuresmsg.layout.dim[0].stride=1
+    featuresmsg.layout.dim[0].label='index'
+    featurespub.publish(featuresmsg)
+
+def publishFeaturesStd(sample):
+    featuresmsgstd.data=sample
+    featuresmsgstd.layout.dim=[MultiArrayDimension()]
+    featuresmsgstd.layout.dim[0].size=len(sample)
+    featuresmsgstd.layout.dim[0].stride=1
+    featuresmsgstd.layout.dim[0].label='index'
+    featuresstdpub.publish(featuresmsgstd)
+
+################################## Loop ############################
+
+rospy.init_node('Featurenode', anonymous=True)
+
+print("Starting FeatureNode")
+
+
+
+ROSRATE=33 #Hz
+rate = rospy.Rate(ROSRATE)
+
+
+msg=Float32()
+while True:
+    feats=np.random.rand(5)
+    if len(feats) != 0:
+        #print(feats)
+        publishFeatures(feats)
+        publishFeaturesStd(feats)
+
+    rate.sleep()
---
+++ python/ros_ws/src/www/README.txt
@@ -0,0 +1,22 @@
+# Some notes of what I did
+
+Sudo apt-get install
+===============================
+# Install apache server to create a local server to host the website
+sudo apt-get install apache2
+sudo a2enmod ssl
+# Install rosbridge to have the capability to run the web socket server
+ros-noetic-rosbridge-suite
+
+# Setup a self-signed certificate to allow ssl communications between browser
+# and robot.
+cp /etc/ssl/private/ssl-cert-snakeoil.key /etc/ssl/certs/
+
+==================================
+
+
+pip install:
+autobahn
+pyOpenSSL
+twisted
+pymongo
